66297f1ce2b5485b68d3163e15a733cdad023356
Scott <scott@simicart.com>
2016-05-25 10:21:34 +0700

    Update Popup

--- Version3.0/simicart-v0.3.5-magento/app/code/local/Simi/Popup/Block/Popup.php
+++ Version3.0/simicart-v0.3.5-magento/app/code/local/Simi/Popup/Block/Popup.php
@@ -30,45 +30,7 @@ class Simi_Popup_Block_Popup extends Mage_Core_Block_Template {
     
 
     public function isMobile() {
-
-        if (!function_exists('getallheaders')) {
-
-            function getallheaders() {
-                $head = array();
-                foreach ($_SERVER as $name => $value) {
-                    if (substr($name, 0, 5) == 'HTTP_') {
-                        $name = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))));
-                        $head[$name] = $value;
-                    } else if ($name == "CONTENT_TYPE") {
-                        $head["Content-Type"] = $value;
-                    } else if ($name == "CONTENT_LENGTH") {
-                        $head["Content-Length"] = $value;
-                    }
-                }
-                return $head;
-            }
-
-        }		
-        $head = getallheaders();
-        if (isset($head['Mobile-App']))
-            return false;
-        if ($_SERVER["HTTP_USER_AGENT"]) {
-            $user_agent = $_SERVER["HTTP_USER_AGENT"];
-			if(Mage::getSingleton('core/session')->getSessionSimiPopup()==null){
-				if (strstr($user_agent, 'iPhone') || strstr($user_agent, 'iPod')){
-				$this->setTemplate('popup/popup.phtml');
-				}			
-				elseif(strstr($user_agent, 'Android')){
-					$this->setTemplate('popup/popup2.phtml');
-				}
-				elseif(strstr($user_agent, 'iPad')){
-					$this->setTemplate('popup/popup1.phtml');
-				}            
-			}            			
-            Mage::getSingleton('core/session')->setSessionSimiPopup(1);
-            return true;            
-        }
-        return FALSE;
+		$this->setTemplate('popup/popup.phtml');
     }
 
 }
